eb6a2122c0f3111b112db1aa39481f03a51b1914
yeonseo <yeonseochoi0717@gmail.com>
2025-09-10 11:45:05 +0900

    chore: images assets 복원

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,24 @@
 import { Stack } from "expo-router";
-import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
 
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow',
+    primary: "tomato",
+    secondary: "yellow",
   },
-}
+};
 
 export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
       <Stack>
-        <Stack.Screen name="index" options={{ title: "홈" }} />
-        <Stack.Screen name="product/[id]" options={{ title: "상품 상세" }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="product/[id]" options={{ headerShown: false }} />
       </Stack>
     </PaperProvider>
   );
