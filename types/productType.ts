7fe828f11e21c3c5203ed9e61ec1cc50e7bab62f
yeonseo <yeonseochoi0717@gmail.com>
2025-09-10 18:22:14 +0900

    feat: 더미 데이터 상품 추가

--- types/productType.ts
+++ types/productType.ts
@@ -1,7 +1,7 @@
 export interface Product {
   id: number;
   title: string;
-  category: "bag" | "pants" | "top";
+  category: "bag" | "pants" | "top" | "outerwear";
   price: number;
   images: string[];
   options?: {colors?: string[]; sizes?: string[]};
