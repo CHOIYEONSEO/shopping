1efe06f7a32a4d9a3fadab78f25f01438b408616
yeonseo <yeonseochoi0717@gmail.com>
2025-09-11 13:51:18 +0900

    style: 상품 카드 높이 수정

--- components/home/ProductCard.tsx
+++ components/home/ProductCard.tsx
@@ -1,21 +1,24 @@
 import { Product } from "@/types/productType";
 import { router } from "expo-router";
+import { Dimensions } from "react-native";
 import { Card, Text } from "react-native-paper";
 
+const COLUMNS_NUM = 2;
+
 export default function ProductCard({ item }: { item: Product }) {
+  const { width } = Dimensions.get("window");
+  const cardWidth = width / COLUMNS_NUM - 16;
+  const cardHeight = cardWidth * 1.6;
+
   return (
     <Card
       onPress={() => router.push(`/product/${item.id}`)}
-      style={{ flex: 1, height: 200 }}
+      style={{ flex: 1, height: cardHeight }}
     >
-      <Card.Cover
-        source={{ uri: item.images[0] }}
-        style={{ height: 120 }}
-        resizeMode="contain"
-      />
+      <Card.Cover source={{ uri: item.images[0] }} resizeMode="contain" />
       <Card.Title title={item.title} subtitle={item.category} />
       <Card.Content>
-        <Text variant="bodyMedium">{`${item.price.toLocaleString()}원`}</Text>
+        <Text variant="labelLarge">{`${item.price.toLocaleString()}원`}</Text>
       </Card.Content>
     </Card>
   );
